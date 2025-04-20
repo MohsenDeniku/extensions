702f5edb06dec8892c1ac27cf199d4d0e0f2ea3f
Mohsen Deniku <mohsen_deniku@icloud.com>
2025-04-20 21:01:12 +0330

    fav icon fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar({handleSearch, darkMode, toggleTheme}) {
 
     return (
         <header className={`${darkMode ? 'dark' : ""}`}>
-            <img className={`logo ${darkMode ? "dark" : ""}`} src="/assets/images/logo.svg" alt="theme icon"/>
+            <img className={`logo ${darkMode ? "dark" : ""}`} src={`/assets/images/logo${darkMode ? "-darkmode" : ""}.svg`} alt="theme icon"/>
             <input onChange={(event) => handleSearch(event)} type="text" placeholder={"Search..."}
                    className={`search-bar ${darkMode ? "dark" : ""}`}/>
             <img className={`theme-btn-${darkMode ? "sun" : "moon"}`} onClick={toggleTheme}
